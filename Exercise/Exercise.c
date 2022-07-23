152d43a3b6e9e16881a084f02665f42439f8a9f5
Orsted <cuong.vm.21@gmail.com>
2022-07-23 19:06:43 +0700

    update sort numbers function

--- Exercise/Exercise.c
+++ Exercise/Exercise.c
@@ -5,7 +5,7 @@
 uint8_t getLengthString(char string[]){
 	uint8_t length = 0U;
 	while(string[length] != '\0'){
-		length++;
+		++length;
 	}
 	return length;
 }
@@ -20,4 +20,34 @@ bool compareTwoStrings(char fristString[], char secondString[]){
 	return true;
 }
 
+/****************************************************************/
+/* Sort Numbers                                                 */
+/****************************************************************/
 
+bool ascending(uint8_t firstNumber, uint8_t secondNumber){
+	return firstNumber > secondNumber;
+}
+
+bool descending(uint8_t firstNumber, uint8_t secondNumber){
+	return firstNumber < secondNumber;
+}
+
+void swapTwoNumbers(uint8_t *firstNumber, uint8_t *secondNumber){
+	if(firstNumber == secondNumber || firstNumber == NULL || secondNumber == NULL) return;
+	*firstNumber = *firstNumber + *secondNumber;
+	*secondNumber = *firstNumber - *secondNumber;
+	*firstNumber = *firstNumber - *secondNumber;
+}
+
+void sortNumbers(uint8_t *array, uint8_t length, bool(*comparison)(uint8_t, uint8_t)){
+	uint8_t tempIndex = 0U;
+	for(uint8_t index = 0U; index < length - 1U; ++index){
+		tempIndex = index;
+		for(uint8_t index2 = index + 1U; index2 < length; ++index2){
+			if(comparison(array[tempIndex], array[index2])){
+				tempIndex = index2;
+			}
+		}
+		swapTwoNumbers(&array[index], &array[tempIndex]);
+	}
+}
--- Exercise/main.c
+++ Exercise/main.c
@@ -1,6 +1,8 @@
 #include "LinkedList.c"
 #include "Exercise.c"
 
+#define LOOP(LENGTH) for(uint8_t index = 0; index < (LENGTH); ++index)
+
 int main(int argc, char const *argv[]){
 
 	printf( "/****************************************************************/\n"
@@ -15,10 +17,32 @@ int main(int argc, char const *argv[]){
 	fgets(firstString, sizeof(firstString), stdin);
 	printf("Enter the second string: ");
 	fgets(secondString, sizeof(secondString), stdin);
-	printf("=> The comparison is %s\n\n", compareTwoStrings(firstString, secondString) ? "correct" : "not correct");
+	printf("=> The comparison is %s\n", compareTwoStrings(firstString, secondString) ? "correct" : "not correct");
 
 
-	printf( "/****************************************************************/\n"
+	printf( "\n/****************************************************************/\n"
+			"/* Sort Numbers                                                 */\n"
+			"/****************************************************************/\n"
+			);
+
+	uint8_t unsortedArray[7U] = {10U, 35U, 1U, UINT8_MAX, 4U, 26U, 10U};
+	uint8_t length = sizeof(unsortedArray) / sizeof(uint8_t);
+	// Ascending order (from small to large)
+	sortNumbers(unsortedArray, length, ascending);
+	printf("Ascending order (from small to large): ");
+	LOOP(length){
+		printf("%d ", unsortedArray[index]);
+	}
+
+	// Descending order (from small to large)
+	sortNumbers(unsortedArray, length, descending);
+	printf("\nDescending order (from large to small): ");
+	LOOP(length){
+		printf("%d ", unsortedArray[index]);
+	}
+
+
+	printf( "\n\n****************************************************************/\n"
 			"/* Linked List                                                  */\n"
 			"/****************************************************************/\n"
 			);
